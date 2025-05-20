4b487af6593d4c718d620967b07bd396387331dd
Alan Camacho <alancamacho491@gmail.com>
2025-05-19 23:03:26 -0600

    Configuracion, controlador y metodo para login

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,22 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::post('login',[LoginController::class, 'login']);
+
+Route::get('clientes/lista', [ClientesController::class, 'listaClientesAPI']);
+Route::get('tecnicos/lista', [ClientesController::class, 'listaTecnicosAPI']);
+Route::post('polizas/lista', [PolizasController::class, 'listaPolizasAPI']);
+
+Route::post('servicio/nuevo', [ServiciosController::class, 'store']);
+Route::post('servicios/eliminar', [ServiciosController::class, 'delete']);
+Route::post('servicio', [ServiciosController::class, 'index']);
+
+Route::get('servicios', [ServiciosController::class, 'list']);
