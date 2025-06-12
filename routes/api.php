2b276d3385ba50ec827700ccbc8d2a4e02883e91
Alan Camacho <alancamacho491@gmail.com>
2025-06-12 00:31:52 -0600

    rutas completadas, sin pruebas

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,7 @@ Route::delete('membresias/{id}',[MembresiasController::class, 'destroy']);//dele
 
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\MembresiasController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
@@ -39,11 +40,19 @@ Route::middleware('auth:sanctum')->get('user', function (Request $request) {
     ]);
 });
 
-// Membresías (todas protegidas)
+// Membresías y usuarios (todas protegidas siksi)
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('membresias', [MembresiasController::class, 'index']);
     Route::post('membresias', [MembresiasController::class, 'store']);
     Route::get('membresias/{id}', [MembresiasController::class, 'show']);
     Route::put('membresias/{id}', [MembresiasController::class, 'update']);
     Route::delete('membresias/{id}', [MembresiasController::class, 'destroy']);
+
+    //usuario (todas protegidas siksi)
+        Route::get('usuarios', [UserController::class, 'index']);
+        Route::post('usuarios', [UserController::class, 'store']);
+        Route::get('usuarios/{id}', [UserController::class, 'show']);
+        Route::put('usuarios/{id}', [UserController::class, 'update']);
+        Route::delete('usuarios/{id}', [UserController::class, 'destroy']);
 });
+
