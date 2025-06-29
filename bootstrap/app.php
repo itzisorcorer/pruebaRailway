0c5a210b905ff43b6fe36f771836ad78afb3e5f7
Alan Camacho <alancamacho491@gmail.com>
2025-06-28 23:06:51 -0600

    posible definitiva a http

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,15 +7,20 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //prueba
-        $middleware->validateCsrfTokens(except: [
-            'stripe/*',
-        ]);
+        
+        // La línea mágica que soluciona el problema en Railway
+        $middleware->trustProxies(at: '*'); 
+
+        // Es posible que tengas otras configuraciones de middleware aquí,
+        // como la verificación del token CSRF, etc. Déjalas como están.
+        // Por ejemplo:
+        // $middleware->validateCsrfTokens(except: [
+        //     'stripe/*',
+        // ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
